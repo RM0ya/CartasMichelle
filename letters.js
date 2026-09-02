fc80b8eefaf949ee928805e9f99a39ac7ef48abb
RodrigöMoya <r.moya@duocuc.cl>
2026-09-02 19:33:12 -0400

    Update letters.js
    update letter nita, panchi

--- letters.js
+++ letters.js
@@ -14,10 +14,29 @@ const letters = [
     sign: 'Siempre tuyo/a'
   },
   {
-    title: 'Un pensamiento rápido,',
+    title: 'Querida hermanita:',
     previewText: 'De Panchi',
-    text: `<p>Eres, sin exagerar, mi parte favorita de cualquier día. Nunca olvides lo especial que eres para mí.</p>`,
-    sign: 'Un abrazo fuerte'
+    text: `<p>Hoy es un día demasiado especial, porque no todos los días se cumplen 18 años. Hoy comienzas una etapa nueva de tu vida,
+              una etapa llena de sueños, cambios, aprendizajes, aventuras y momentos que estoy segura de que vas a recordar para siempre. 
+              Y yo no podía dejar pasar este día sin decirte todo lo que significas para mí.
+              Primero que todo, quiero desearte un muy feliz cumpleaños. Espero que este nuevo año de vida venga lleno de cosas bonitas,
+              de personas que te quieran de verdad,
+              de momentos que te hagan sonreír y de sueños que poco a poco puedas cumplir.
+              Te mereces todo lo lindo que la vida pueda darte.
+              A veces quizás no te lo digo, o quizás no soy muy buena demostrando lo que siento, pero quiero que sepas que te quiero muchísimo.
+              Eres una persona muy importante para mí y, aunque 
+              como hermanas seguramente hemos tenido nuestras peleas, diferencias, enojos y momentos en los que no nos hemos entendido, 
+              nada de eso cambia el cariño enorme que te tengo.
+              Hemos compartido tantos momentos juntas. Algunos han sido felices, otros no tanto, 
+              pero todos forman parte de nuestra historia. Hemos reído, hemos conversado, 
+              hemos discutido por cosas pequeñas y también hemos estado ahí en momentos importantes.
+              Y aunque el tiempo pase y cada una vaya creciendo y tomando su propio camino, siempre vas a ser mi hermana y siempre voy a guardar un lugar muy especial para ti en mi corazón.
+              Hoy que cumples 18 años, quiero que mires hacia atrás y recuerdes todo lo que has vivido. 
+              Mira cuánto has crecido, cuánto has aprendido y todas las cosas que has logrado superar. 
+              Quizás todavía no te das cuenta de todo lo que eres capaz de hacer, pero espero que nunca dudes de ti misma. 
+              Te quiero mucho y sige cumpliendo más años mas
+           </p>`,
+    sign: 'Con amor Panchi'
   },
   {
     title: '',
@@ -38,6 +57,16 @@ const letters = [
     sign: ''
   },
   {
+    title: '¡Feliz cumpleaños, mi niña hermosa!',
+    previewText: 'De Abuela Nita',
+    text: `<p>  Que sigas cumpliendo muchos más junto a tu padres y hermanos; 
+                yo, tu abuela, me siento muy orgullosa de la niña que eres. 
+                Sé que tienes muchos sueños en tu vida y sé que los vas a lograr; 
+                espero verte convertida en una gran mujer. Mi niña, no olvides que la amo con todo mi corazón; 
+                </p>`,
+    sign: 'Con amor tu abuela Nita.'
+  },
+  {
     title: '',
     previewText: 'De Sally',
     text: `<p> </p>`,
@@ -49,6 +78,7 @@ const letters = [
     text: `<p> </p>`,
     sign: ''
   },
+  
   {
     title: '',
     previewText: 'De Maikel',
@@ -68,12 +98,7 @@ const letters = [
     sign: ''
   },
  
-  {
-    title: '',
-    previewText: 'De Bernarda',
-    text: `<p> </p>`,
-    sign: ''
-  },
+  
   {
     title: 'De Don Mauri',
     previewText: 'De ',
